d905259e1bef3429f965962e2b840c18335a85a9
xanayax <x.anaya@sapalomera.cat>
2022-05-29 14:44:47 +0200

    error deploy

--- projecte_cine/settings.py
+++ projecte_cine/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'd^xla98wopzsnjwhro74tw4hj*^wk1%jnc-!aqd#_e=r#=k1*m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
