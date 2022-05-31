826cdb7bd28738e70028052e0db79f072595c7ac
xanayax <x.anaya@sapalomera.cat>
2022-05-31 22:51:05 +0200

    error urls

--- projecte_cine/settings.py
+++ projecte_cine/settings.py
@@ -131,8 +131,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+''''
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,  'Apps/gestio_cine/static/static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+'''
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'Apps/gestio_cine/static/static')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
